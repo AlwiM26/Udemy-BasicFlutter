5c3b5aba5377b789639677031303d8e4a17fe5de
AlwiM26 <alwimhd01@gmail.com>
2020-03-16 14:55:27 +0700

    Added a layout with bunch of buton without anny function on the onpressed property

--- lib/main.dart
+++ lib/main.dart
@@ -6,21 +6,40 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext c) {
+    //  List of questions with type of String
+    List<String> questions = [
+      'What\'s your favorite color? ',
+      'What\'s your favorite music genre ? ',
+      'What\'s your favorit movie genre ? '
+    ];
     return MaterialApp(
-      title: "My App",
+      title: 'My App',
       home: Scaffold(
         appBar: AppBar(
-          title: Text("My First Apps"),
+          title: Text('My First Apps'),
         ),
-        body: Center(
-          child: Text(
-            "Hello World!",
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-              color: Colors.green,
+        //  Column just is a vertical layout, so we can have a multiple widget inside of the body property
+        body: Column(
+          //  Put all the widget that we want inside the angle bracket '[]'
+          children: [
+            Text(questions[0]),
+            RaisedButton(
+              onPressed: null,
+              child: Text("Answer 1"),
             ),
-          ),
+            RaisedButton(
+              onPressed: null,
+              child: Text("Answer 2"),
+            ),
+            RaisedButton(
+              onPressed: null,
+              child: Text("Answer 3"),
+            ),
+            RaisedButton(
+              onPressed: null,
+              child: Text("Answer 4"),
+            ),
+          ],
         ),
       ),
     );
